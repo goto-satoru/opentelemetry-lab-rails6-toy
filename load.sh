2387f02987c7931673dc7c71df9e488e0f578a27
GOTO Satoru <sagoto@cisco.com>
2022-07-23 11:31:05 +0900

    service.name -> otel-collector-config.yaml

--- load.sh
+++ load.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-curl -I http://localhost:3000/users/
-curl -I http://localhost:3000/users/1
-curl -I http://localhost:3000/microposts
-curl -I http://localhost:3000/microposts/1
+while true
+do
+    curl -I http://localhost:3000/users/
+    curl -I http://localhost:3000/users/1
+    curl -I http://localhost:3000/microposts
+    curl -I http://localhost:3000/microposts/1
+
+    wait=$(($RANDOM % 10))
+    echo "wait: ${wait} seconds"
+    sleep $wait
+done
